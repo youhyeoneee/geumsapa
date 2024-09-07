86a3fc121f66f7d1535800cfef2116c7e4dd537c
youhyeoneee <khukyh2018@khu.ac.kr>
2024-09-08 00:58:14 +0900

    comment: 회원가입 controller, service 메서드 주석 추가

--- auth-server/src/main/java/com/yhkim/domain/user/controller/UserController.java
+++ auth-server/src/main/java/com/yhkim/domain/user/controller/UserController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,16 @@ import static com.yhkim.util.ApiUtils.success;
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
-
+    
     private final UserService userService;
-
+    
+    /**
+     * 회원 가입
+     *
+     * @param signupUserRequest 가입할 회원 정보
+     * @return 상태 코드 201 CREATED
+     * @throws MethodArgumentNotValidException 회원가입 필드가 형식에 안맞을 경우 발생
+     */
     @PostMapping
     public ResponseEntity<ApiUtils.SuccessResponse<SignupUserResponse>> signup(@Valid @RequestBody SignupUserRequest signupUserRequest) {
         return success(HttpStatus.CREATED, "Success to signup", userService.signup(signupUserRequest));
--- auth-server/src/main/java/com/yhkim/domain/user/service/UserServiceImpl.java
+++ auth-server/src/main/java/com/yhkim/domain/user/service/UserServiceImpl.java
@@ -15,20 +15,34 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
+    
     private final UserRepository userRepository;
-
+    
+    
+    /**
+     * 회원 가입 처리
+     *
+     * @param signupUserRequest 회원 가입 요청 정보
+     * @return SignupUserResponse 가입된 사용자 정보 응답
+     * @throws CustomException 계정이 이미 존재하는 경우 발생
+     */
     @Override
     @Transactional
     public SignupUserResponse signup(SignupUserRequest signupUserRequest) {
         if (isUserExist(signupUserRequest.getUsername())) {
             throw new CustomException(ErrorCode.USERNAME_ALREADY_EXIST);
         }
-
+        
         User savedUser = userRepository.save(signupUserRequest.toEntity());
         return SignupUserResponse.fromEntity(savedUser);
     }
-
+    
+    /**
+     * 계정 존재 여부 확인
+     *
+     * @param username 확인할 계정
+     * @return boolean 존재 여부 (true: 존재함, false: 존재 하지 않음)
+     */
     private boolean isUserExist(String username) {
         return userRepository.findByUsername(username).isPresent();
     }

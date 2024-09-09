1047a2bb3780987cdd2d800ebdec7e0961dd0a78
youhyeoneee <khukyh2018@khu.ac.kr>
2024-09-09 10:43:41 +0900

    feat: refresh token entity 및 repository 추가

---
+++ auth-server/src/main/java/com/yhkim/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.yhkim.auth.repository;
+
+import com.yhkim.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
+    
+    Optional<RefreshToken> findByUsername(String username);
+}

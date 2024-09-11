86832f773d34a348f9b8ff58e91c8161731be222
youhyeoneee <khukyh2018@khu.ac.kr>
2024-09-11 18:27:23 +0900

    chore: Product 초기 데이터 생성

---
+++ resource-server/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `geumsapa-resource`.products
+    (id, name, code, created_at)
+VALUES (1, 'Gold 99.99%', 'GOLD_99_99', '2024-09-09 12:00:00'),
+       (2, 'Gold 99.9%', 'GOLD_99_9', '2024-09-09 12:00:00');
